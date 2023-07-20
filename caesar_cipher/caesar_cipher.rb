7d358b5a071e54e876650269fad2569e70e9d5c0
Reiyirak <pdsmej@gmail.com>
2023-07-19 21:54:58 -0600

    Finished overall method, but i need to fix some things
    The things i need to fix are when the letter z is on the given word(that the user
    wants to encrypt), because the index needs to go back to a and then keep searching
    Also when the user gives an incorrect format when values are asked, like the word
    and the shift factor.

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -3,8 +3,31 @@
 def cipher(string, shift)
   alphabet = [*('a'..'z')]
   old_string = string.split('')
-  p old_string
-  p alphabet
+  new_string = do_encyption(alphabet, old_string, shift)
+  new_string.join
 end
 
-cipher('hello', 5)
+def do_encyption(alphabet, string_array, shift)
+  new_string = []
+  string_array.map do |char|
+    alphabet.each_with_index do |letter, index|
+      if char == letter && char == char.upcase
+        new_letter = alphabet[index + shift].upcase
+        new_string.push(new_letter)
+        break
+      elsif char == letter
+        new_letter = alphabet[index + shift]
+        new_string.push(new_letter)
+        break
+      end
+    end
+  end
+  new_string
+end
+
+puts 'What is the word you want to encrypt?'
+word = gets.chomp
+puts 'What is the shift factor?'
+shift = gets.chomp.to_i
+encrypted_word = cipher(word, shift)
+puts "\nThe new encrypted word is #{encrypted_word}"
