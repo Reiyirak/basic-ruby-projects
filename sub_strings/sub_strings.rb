a7130fc40dad751a4fa219dae7f5cab893f44eaf
Reiyirak <pdsmej@gmail.com>
2023-07-28 10:44:03 -0600

    I read the instructions wrong, is only one string not unlimited strings

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,27 +1,16 @@
 # frozen_string_literal: true
 
-def args_validation(args)
-  # Return false if there's not at least 2 arguments(the string and the array)
-  return false if args.length < 2
-
+def args_validation(words, dictionary)
   # Return false if the last argument is not an Array
-  last_argument = args.last
-  return false unless last_argument.is_a?(Array) && last_argument.all? { |item| item.is_a?(String) }
+  return false unless dictionary.is_a?(Array) && dictionary.all? { |item| item.is_a?(String) }
 
   # Return true if all the arguments except the last one are Strings
-  args[0...-1].all? { |item| item.is_a?(String) }
+  return false unless words.is_a?(String)
 end
 
-def substrings(*args)
-  validation = args_validation(args)
-  unless validation
-    return puts "
-      The last argument must be an Array of strings and all the other arguments\n
-      must be strings
-    "
-  end
-
-  return puts 'good job'
+def substrings(words, dictionary)
+  validation = args_validation(words, dictionary)
+  return puts 'Make sure to give the appropiate arguments' unless validation
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
