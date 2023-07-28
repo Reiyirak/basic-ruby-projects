88b25b9fb53a39bf5d9b77353186c12ee60f32c4
Reiyirak <pdsmej@gmail.com>
2023-07-27 22:12:31 -0600

    Setup validations for the arguments

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
+def args_validation(args)
+  # Return false if there's not at least 2 arguments(the string and the array)
+  return false if args.length < 2
+
+  # Return false if the last argument is not an Array
+  last_argument = args.last
+  return false unless last_argument.is_a?(Array) && last_argument.all? { |item| item.is_a?(String) }
+
+  # Return true if all the arguments except the last one are Strings
+  args[0...-1].all? { |item| item.is_a?(String) }
+end
+
 def substrings(*args)
-  args.each { |arg| p arg }
+  validation = args_validation(args)
+  unless validation
+    return puts "
+      The last argument must be an Array of strings and all the other arguments\n
+      must be strings
+    "
+  end
+
+  return puts 'good job'
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
