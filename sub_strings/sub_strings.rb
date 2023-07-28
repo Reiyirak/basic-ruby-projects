da4b30a20e8cf6bef4e4ad8d3b3a5717ef6760ac
Reiyirak <pdsmej@gmail.com>
2023-07-28 11:13:55 -0600

    Finished substrings project

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -6,12 +6,33 @@ def args_validation(words, dictionary)
 
   # Return true if all the arguments except the last one are Strings
   return false unless words.is_a?(String)
+
+  true
 end
 
 def substrings(words, dictionary)
+  results = Hash.new(0)
   validation = args_validation(words, dictionary)
   return puts 'Make sure to give the appropiate arguments' unless validation
+
+  # The case must be insensitive so we can just lowercase all the words
+  new_words = words.downcase
+
+  # Now we iterate each of the words inside the dictionary
+  dictionary.each do |word|
+    # We are gonig to use the method scan for each word we are iterating
+    quantity = new_words.scan(word).length
+
+    # With the quantity we are getting how many times a subtring in the dictionary
+    # match the pattern of the string words
+    # Now we need to add it to the results hash, with the key beign the iterated word
+    # and the value will be the quantity
+    results[word] = quantity unless quantity.zero?
+  end
+
+  # Finally we return the hash so that we can print it to the user
+  results
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
