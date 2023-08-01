f07be3b1bfdf48aaedb5caa343107687a8bc806e
Reiyirak <pdsmej@gmail.com>
2023-07-31 23:06:27 -0600

    Finished bubble sort project
    With this commit the basic ruby projects are completed

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 def bubble_sort(numbers)
+  # Validation, numbers is an array of integers
+  return p false unless numbers.is_a?(Array) && numbers.all? { |number| number.is_a?(Integer) }
+
+  # Placeholder
+  change = true
+
+  while change == true
+    change = false
+    numbers.each_with_index do |number, index|
+      break if index == numbers.length - 1
+
+      if numbers[index] > numbers[index + 1]
+        numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
+        change = true
+      end
+    end
+  end
+
   p numbers
 end
 
